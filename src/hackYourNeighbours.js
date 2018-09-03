ece39fafdff12719628f641f52e993e1b476d9a7
Zerneax <d.chris59@gmail.com>
2018-09-03 22:02:17 +0200

    add receptacles

--- src/hackYourNeighbours.js
+++ src/hackYourNeighbours.js
@@ -5,7 +5,14 @@ var ctx = canvas.getContext("2d");
 var ballX = canvas.width/2;
 var ballY = 10;
 
+// receptacle informations
+var receptacles = [];
+var widthReceptacle = canvas.width/3;
+var heightReceptacle = 20;
+
 drawBall();
+initReceptacle();
+drawReceptacles();
 
 function drawBall() {
   ctx.beginPath();
@@ -13,4 +20,45 @@ function drawBall() {
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function initReceptacle() {
+  for(var i = 0; i < 3; i ++)
+  {
+
+    var r = {
+      x: i * widthReceptacle,
+      y: canvas.height - heightReceptacle,
+      color: returnColor(i)
+    }
+    receptacles[i] = r;
+  }
+}
+
+function returnColor(i) {
+  switch (i) {
+    case 0:
+      return "#ff0000";
+      break;
+    case 1:
+      return "#00ff00";
+      break;
+    case 2:
+      return "#0000ff";
+      break;
+    default:
+    return "#ffffff";
+
+  }
+}
+
+function drawReceptacles() {
+  for(var i = 0; i < 3; i ++)
+  {
+    ctx.beginPath();
+    ctx.rect(receptacles[i].x, receptacles[i].y, widthReceptacle, heightReceptacle);
+    ctx.fillStyle = receptacles[i].color;
+    ctx.fill();
+    ctx.closePath();
+  }
 }
